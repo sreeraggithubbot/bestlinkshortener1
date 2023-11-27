a51db801f2a2157660a063fd199d9bc3f5fd12e9
samshine <99365623+sreeraggithubbot@users.noreply.github.com>
2023-11-28 03:27:51 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -116,18 +116,18 @@ async def private_receive_handler(c: Client, m: Message):
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL) 
         stream_links = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}" 
         online_links = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}" 
-        stream_link = get_shortlink(stream_links) 
+        stream_link = get_shortlink(online_links) 
         online_link = get_shortlink(online_links)
         tutorial = f"{Var.TUTORIAL_URL}"
   
         msg_text ="""<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WILL NOT EXPIRE UNTIL I DELETE</b>""" 
   
-        await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {online_link}", disable_web_page_preview=True,  quote=True) 
+        await log_msg.reply_text(text=f"{online_link}", disable_web_page_preview=True,  quote=True) 
         await m.reply_text( 
-            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link), 
+            text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, online_link), 
             quote=True, 
             disable_web_page_preview=True, 
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM 🖥", url=stream_link), #Stream Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM 🖥", url=online_link), #Stream Link
                                                InlineKeyboardButton('DOWNLOAD 📥', url=online_link)],
                                             [InlineKeyboardButton("HOW TO OPEN LINK", url=tutorial)]]) #Download Link
        )
@@ -157,10 +157,10 @@ async def channel_receive_handler(bot, broadcast):
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL) 
         stream_links = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}" 
         online_links = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}" 
-        stream_link = get_shortlink(stream_links) 
+        stream_link = get_shortlink(online_links) 
         online_link = get_shortlink(online_links) 
         await log_msg.reply_text( 
-            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}", 
+            text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {online_link}", 
             quote=True 
         ) 
         await bot.edit_message_reply_markup( 
@@ -168,7 +168,7 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.id, 
             reply_markup=InlineKeyboardMarkup( 
                 [ 
-                    [InlineKeyboardButton("🖥STREAM ", url=stream_link), 
+                    [InlineKeyboardButton("🖥STREAM ", url=online_link), 
                      InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=online_link)]  
                 ] 
             ) 
